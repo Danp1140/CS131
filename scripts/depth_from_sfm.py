ef64dbb270dcc3727b099979c8050bab8943e423
Danp1140 <paavola.dan@gmail.com>
2026-06-06 16:38:18 -0700

    Improved depth_from_sfm

--- scripts/depth_from_sfm.py
+++ scripts/depth_from_sfm.py
@@ -2,6 +2,8 @@
 # gridding them, and looking for the lowest change in depth gradient (windowed average) starting from the bottom
 #
 # Arg 1 is SfM output folder
+# Arg 2 is the image name prefix (e.g., imgs/carlsbad_2024/carlsbad_)
+# Arg 3, if provided, is a PLY point cloud for a dense reconstruction
 #
 # Makes plots of
 # - Raw projected z coord gridded
@@ -19,12 +21,14 @@ import cv2 as cv
 from pathlib import Path
 from plyfile import PlyData
 
+img_idx = 4
+
 recon = pcm.Reconstruction(sys.argv[1])
 
 pc = o3d.geometry.PointCloud()
 
-if len(sys.argv) > 2 and Path(sys.argv[2]).is_file() and sys.argv[2][-4:] == ".ply":
-    data = PlyData.read(sys.argv[2])
+if len(sys.argv) > 3 and Path(sys.argv[3]).is_file() and sys.argv[3][-4:] == ".ply":
+    data = PlyData.read(sys.argv[3])
     pc.points = o3d.utility.Vector3dVector(np.array([[p["x"], p["y"], p["z"]] for p in data["vertex"]]))
     pc.colors = o3d.utility.Vector3dVector(np.array([[p["red"], p["green"], p["blue"]] for p in data["vertex"]]) / 256)
 
@@ -35,15 +39,15 @@ else:
 pc.estimate_normals()
 
 intr = np.eye(4)
-intr[:3, :3] = recon.image(1).camera.calibration_matrix()
-extr = recon.image(1).cam_from_world().matrix()
+intr[:3, :3] = recon.image(img_idx).camera.calibration_matrix()
+extr = recon.image(img_idx).cam_from_world().matrix()
 extr = np.vstack((extr, [0, 0, 0, 1]))
 
 # pc = pc.transform(np.linalg.inv(extr))
-np.asarray(pc.points)[:, :2] = np.array([recon.image(1).project_point(p) for p in pc.points])
+np.asarray(pc.points)[:, :2] = np.array([recon.image(img_idx).project_point(p) for p in pc.points])
 
-# cl, ind = pc.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
-# pc = pc.select_by_index(ind)
+cl, ind = pc.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+pc = pc.select_by_index(ind)
 
 points = np.array(pc.points)
 
@@ -77,47 +81,53 @@ def genTransects(n):
     min_y = np.min(np.asarray(pc.points)[:, 1])
     max_y = np.max(np.asarray(pc.points)[:, 1])
     min_points = np.zeros(n)
-    N = 50
-    k_w = 15
+    N = 128
+    k_w = 5
     out = np.zeros((n,N-2*k_w))
-    out_dif = np.zeros((n, N-2*k_w-1))
+    out_dif = np.zeros((n, N-4*k_w-1))
     for i in range(n):
         trans_origins[i, :] = [min_x + (i+1) * del_x / (n+1), max_y]
-        plt.plot(trans_origins[i, 0] * np.ones(2), [0, max_y])
+        plt.plot(trans_origins[i, 0] * np.ones(2), [0, max_y], ":", color="k")
         trans_points = []
         for p in pc.points:
-            if abs(p[0] - trans_origins[i, 0]) < del_x / (n+1):
+            if abs(p[0] - trans_origins[i, 0]) < del_x / (n+1) / 2:
                 trans_points += [p]
         trans_points = np.array(trans_points)
         trans_points = trans_points[np.argsort(trans_points[:, 1]), :]
-        #plt.figure()
-        #plt.plot(trans_points[:, 2])
-        # out[i, :] = np.interp(np.linspace(np.min(trans_points[:, 1]), np.max(trans_points[:, 1]), N), trans_points[:, 1], trans_points[:, 2])
-        temp = np.interp(np.linspace(0, len(trans_points[:, 1]), N), np.arange(0, len(trans_points[:, 1])), trans_points[:, 2])
-        out[i, :] = np.convolve(temp, np.ones(2*k_w+1), mode="valid")
-        out_dif[i, :] = np.diff(out[i, :])
+        min_y_trans = np.min(trans_points[:, 1])
+        max_y_trans = np.max(trans_points[:, 1])
+        temp = sp.interpolate.interp1d(trans_points[:, 1], trans_points[:, 2])(np.linspace(min_y_trans, max_y_trans, N))
+        out[i, :] = np.convolve(temp, np.ones(2*k_w+1)/(2*k_w+1), mode="valid")
+        out_dif[i, :] = np.convolve(np.diff(out[i, :]), np.ones(2*k_w+1) / (2*k_w+1), mode="valid")
         out_dif[i, :] = np.abs(out_dif[i, :])
-        # min_points[i] = trans_points[sp.signal.argrelextrema(trans_points[:, 2], np.greater)[0][-1], 2]
-        min_points[i] = min_y + (max_y - min_y) * (1 - sp.signal.argrelextrema(out_dif[i, :], np.greater)[0][0] / len(out_dif[i, :]))
-    plt.plot(trans_origins[:, 0], min_points)
+        min_idx = sp.signal.argrelextrema(out_dif[i, :], np.greater)
+        if len(min_idx[0]) > 0:
+            min_points[i] = min_y_trans + (max_y_trans - min_y_trans) * float(min_idx[0][-1]) / float(len(out_dif[i, :]))
+    plt.plot(trans_origins[:, 0], min_points, c="red")
     print(min_points)
     return out, out_dif
  
 
-# img = cv.imread("imgs/miramar_2013/miramar_1.png")
-img = cv.imread("imgs/carlsbad_2024/carlsbad_1.png")
+img = cv.imread(f"{argv[2]}{int(img_idx)}.png")
 
 plt.figure()
 
 plt.imshow(img[:, :, ::-1])
 gm = img.shape[0] * gm / 500
-plt.plot([0, img.shape[1]], [gm, gm])
-plt.scatter(np.asarray(pc.points)[:, 0], np.asarray(pc.points)[:, 1], c=np.asarray(pc.colors))
-#trans, dtrans = genTransects(10)
+n=16
+trans, dtrans = genTransects(n)
 
-# for i in range(10):
-    # plt.figure()
-    # plt.plot(dtrans[i, :])
+# below may be uncommented to see each individual transect plotted
+"""
+for i in range(n):
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.plot(trans[i, :])
+    plt.title("Transect z")
+    plt.subplot(1, 2, 2)
+    plt.plot(dtrans[i, :])
+    plt.title("Transect dz")
+"""
 
 plt.show()
 
